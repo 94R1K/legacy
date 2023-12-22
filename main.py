7868b9c8aa1a49260780437d569cbcaaf1005a01
94R1K <real-man228@yandex.ru>
2023-12-22 11:32:39 +0300

    update

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main(count_send: int):
     url = ("https://www.wildberries.ru/catalog/0/search.aspx?search=%D0%BC%D0%"
            "B5%D1%88%D0%B0%D0%BB%D0%BA%D0%B0%20%D0%B4%D0%BB%D1%8F%20%D1%80%D0%"
            "B0%D1%81%D1%82%D0%B2%D0%BE%D1%80%D0%B0")
-    successful_requests = 0
-    failed_requests = 0
+    successful_requests = 1
+    failed_requests = 1
 
     driver = webdriver.Chrome()
 
@@ -26,9 +26,8 @@ def main(count_send: int):
         try:
             # Загружаем страницу
             driver.get(url)
-            successful_requests += 1
             print(f"#{successful_requests} Успешный запрос: {driver.title}")
-
+            time.sleep(5)
             while True:
                 driver.execute_script(f"window.scrollTo(0, {current_height});")
                 time.sleep(2)
@@ -39,8 +38,8 @@ def main(count_send: int):
                     "document.documentElement.scrollHeight, "
                     "document.documentElement.offsetHeight );"
                 )
-
                 if new_height == current_height:
+                    successful_requests += 1
                     break
 
                 # Обновить текущую высоту
@@ -49,8 +48,11 @@ def main(count_send: int):
         except WebDriverException as e:
             failed_requests += 1
             print(f"#{failed_requests} Ошибка при запросе: {e}")
-        driver.quit()
+    driver.quit()
+
+    print(f"\nИтого успешных запросов: {successful_requests - 1}")
+    print(f"Итого запросов с ошибкой: {failed_requests - 1}")
 
 
-print(f"\nИтого успешных запросов: {successful_requests}")
-print(f"Итого запросов с ошибкой: {failed_requests}")
+if __name__ == '__main__':
+    main(3)
